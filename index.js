77356a47aa090f86b9ab5af8a21ead80a4d15dd0
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-02 14:36:50 -0600

    all user and post crud operations workign except for post /:id/posts

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // code away!
+const express = require('express');
 const server = require('./server.js');
+const postsRouter = require('./posts/postRouter');
+const usersRouter = require('./users/userRouter');
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter)
+server.use('/api/users', usersRouter)
 
 server.listen(9998, () => {
   console.log('\n* Server Running on http://localhost:9998 *\n');
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,91 @@
-const express = 'express';
+const express = require('express');
+const userModel = require('./userDb');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-
+const userData = req.body;
+userModel
+.insert(userData)
+.then(user=>{
+    res.status(201).json(user)
+})
+.catch(error=>{
+    res.status(500).json({ errorMessage: "User could not be created."})
+})
 });
 
 router.post('/:id/posts', (req, res) => {
-
+const id = req.params.id;
+const userPost = req.body;
+userModel
+.insert(id, userPost)
+.then(user=>{
+    res.status(200).json(user)
+})
+.catch(error=>{
+    res.status(500).json({ errorMessage: "This post could not be created."})
+})
 });
 
 router.get('/', (req, res) => {
-
+    userModel
+    .get()
+    .then(users=> {
+        res.json(users);
+    })
+    .catch(error=>{
+        res.status(500).json({ errorMessage: "User data could not be retrieved."})
+    })
 });
 
 router.get('/:id', (req, res) => {
-
+const id = req.params.id;
+userModel
+.getById(id)
+.then(users=>{
+    res.status(200).json(users)
+})
+.catch(error=>{
+    res.status(500).json({ errorMessage: "The posts information could not be retrieved"})
+})
 });
 
 router.get('/:id/posts', (req, res) => {
-
+const id=req.params.id;
+userModel
+.getUserPosts(id)
+.then(users=>{
+    res.status(201).json(users)
+})
+.catch(error=>{
+    res.status(500).json({ errorMessage: "Could not get user posts."})
+})
 });
 
 router.delete('/:id', (req, res) => {
-
+id= req.params.id;
+userModel
+.remove(id)
+.then(users=>{
+    res.status(204).json(users)
+})
+.catch(error=>{
+    res.status(500).json({ errorMessage: "User data could not be removed."})
+})
 });
 
 router.put('/:id', (req, res) => {
-
+const id = req.params.id;
+const changes = req.body;
+userModel
+.update(id, changes)
+.then(users=>{
+    res.status(201).json(users)
+})
+.catch(error=>{
+    res.status(500).json({ errorMessage: "user could not be updated."})
+})
 });
 
 //custom middleware
